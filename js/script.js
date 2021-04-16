e6bd8cbadde1b445edc31b9c69e9e7130c41763b
yeisoncr <yeisoncr@ufps.edu.co>
2021-04-16 07:58:13 -0500

    carga de imagenes con js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const cuadro = document.getElementById("cuadro")
+for (let index = 1; index < 14; index++) {
+    const div = document.createElement("div")
+    const img = document.createElement("img")
+    img.setAttribute("src", 'Img/' + index + '.png')
+    div.setAttribute("data-carta", index)
+    div.setAttribute("id", "btn-carta")
+    div.setAttribute("class", "btn-carta col")
+    div.appendChild(img)
+    cuadro.appendChild(div)
+}
