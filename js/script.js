e89056f64eebbc269f8d11bc12ad6c32ef754026
yeisoncr <72077707+yeisoncr@users.noreply.github.com>
2021-04-17 17:41:04 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const cuadro = document.getElementById("cuadro")
 for (let index = 1; index < 14; index++) {
     const div = document.createElement("div")
     const img = document.createElement("img")
-    img.setAttribute("src", 'Img/' + index + '.png')
+    img.setAttribute("src", 'img/' + index + '.png')
     div.setAttribute("data-carta", index)
     div.setAttribute("id", "btn-carta")
     div.setAttribute("class", "btn-carta col")
